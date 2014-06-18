24524cb99965a7c60baa84ea9c1cef7e9a77f0a5
Murtaza Kheriwala <murtaza.kheriwala@gmail.com>
2014-06-18 22:01:47 +1000

    test commit

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -55,7 +55,7 @@ describe "StaticPages" do
 =begin
   describe "GET /static_pages" do
     it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers..
       get static_pages_index_path
       response.status.should be(200)
     end
