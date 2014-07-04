3fc9def83e7b34099a1273796e3a3cf563416ec3
Murtaza Kheriwala <murtaza.kheriwala@gmail.com>
2014-07-04 20:57:14 +1000

    Added signup functionality.

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name   "Murtaza Kheriwala"
+    email  "murtaza@email.com"
+    password "abc456"
+    password_confirmation "abc456"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,7 +4,7 @@ describe "UserPages" do
 
   subject { page }
 
-  describe "Signup_page" do
+  describe "signup page" do
     before { visit signup_path }
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
@@ -13,5 +13,47 @@ describe "UserPages" do
       get user_pages_index_path
       response.status.should be(200)
 =end
+  end
+
+  describe "profile page" do
+    # Replace with code to make a user variable
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  end
+
+  describe "signup" do
+    before { visit signup_path }
+    let(:submit) { "Create my account" }
+
+    describe "invalid signup" do
+      it "should not create a user" do
+        expect { click_button submit }.not_to change(User, :count)
+      end
+      describe "after submission" do
+        before { click_button submit }
+        it { should have_title('Sign up') }
+        it { should have_content('error') }
+      end
+    end
+
+    describe "valid signup" do
+      before do
+        fill_in "Name",         with: "Example User"
+        fill_in "Email",        with: "example@user.com"
+        fill_in "Password",     with: "PassworD"
+        fill_in "Confirmation", with: "PassworD"
+      end
+      it "should create a user" do
+        expect { click_button submit }.to change(User, :count).by(1)
+      end
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: "example@user.com") }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
+    end
   end
 end
